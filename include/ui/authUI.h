51ae0f2bdfbe7c198b862dce60df914ab73fe2fa
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-01-14 14:12:09 +0530

    access specifiers corrected and explicit constructor introduced

--- include/ui/authUI.h
+++ include/ui/authUI.h
@@ -6,11 +6,11 @@ class AuthManager;
 class AuthUI {
     private:
         AuthManager& authManager;
-    public:
-        AuthUI(AuthManager& manager);
-        void authManagerMenu();
         void newMasterSignUp();
         void oldMasterSignIn();
+    public:
+        explicit AuthUI(AuthManager& manager);
+        void authManagerMenu();
 };
 
 #endif
